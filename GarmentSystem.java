9f07c126f6cbff683a81ffccb8322bcb96a9bdad
hridoy-56 <hridoy23105101392@diu.edu.bd>
2024-11-05 19:30:30 +0600

    Add Garment Class

---
+++ GarmentSystem.java
@@ -0,0 +1,34 @@
+
+package garmentsystem;
+
+
+class Garment {
+
+    public String id;
+    public String name;
+    public String description;
+    public String size;
+    public String color;
+    public double price;
+    public int stockQuantity;
+
+    void updateStock(int quantity) {
+        this.stockQuantity = quantity;
+    }
+
+    double calculateDiscountPrice(double discountPercentage) {
+        double discount = price * (discountPercentage / 100);
+        return discount;
+    }
+}
+
+
+
+public class GarmentSystem {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
