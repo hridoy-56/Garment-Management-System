789950e742e7bd6e75765f42713c36c729976cd3
hridoy-56 <hridoy23105101392@diu.edu.bd>
2024-11-05 19:59:59 +0600

    Supplier, Order, Customer, and Inventory class Added

--- GarmentSystem.java
+++ GarmentSystem.java
@@ -1,6 +1,8 @@
-
 package garmentsystem;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
 
 class Garment {
 
@@ -35,13 +37,100 @@ class Fabric {
     }
 }
 
+class Supplier {
 
+    public String id;
+    public String name;
+    public String contactInfo;
+    //List
+    List<Fabric> suppliedFabric = new ArrayList<>();
+
+    void addFabric(Fabric fabric) {
+        suppliedFabric.add(fabric);
+    }
+
+    List<Fabric> getSuppliedFabrics() {
+        return suppliedFabric;
+    }
+}
+
+class Order {
+
+    public String orderId;
+    public Date orderDate;
+    public List<Garment> garments = new ArrayList<>();
+    private double totalAmount;
+
+    void addGarment(Garment garment) {
+        garments.add(garment);
+    }
+
+    double calculateTotalAmount() {
+        for (Garment g : garments) {
+            totalAmount = totalAmount + g.price;
+        }
+        return totalAmount;
+    }
+
+    void printOrderDetails() {
+        System.out.println("--------------------------");
+        System.out.println("Order Details");
+        System.out.println("--------------------------");
+        for (Garment g : garments) {
+            System.out.println("Name: " + g.name);
+            System.out.println("Price: " + g.price);
+            System.out.println("Description: " + g.description);
+            System.out.println("--------------------------");
+        }
+    }
+}
+
+class Customer {
+
+    public String customerId;
+    public String name;
+    public String email;
+    public String phone;
+
+    void placeOrder(Order order) {
+        order.printOrderDetails();
+        System.out.println("Order Placed");
+    }
+
+}
+
+class Inventory {
+
+    List<Garment> garments;
+
+    void addGarment(Garment garment) {
+        garments.add(garment);
+    }
+
+    void removeGarment(String id) {
+        garments.remove(id);
+    }
+
+    Garment findGarment(String id) {
+        for (Garment g : garments) {
+            if (g.id == id) {
+                return g;
+            }
+        }
+        return null;
+    }
+}
 
 public class GarmentSystem {
 
-    
     public static void main(String[] args) {
-        
+        Garment g1 = new Garment();
+        g1.name = "Silk";
+        g1.description = "Good Product";
+        g1.price = 600;
+        double x = g1.calculateDiscountPrice(10);
+        System.out.println(x);
+
     }
-    
+
 }
